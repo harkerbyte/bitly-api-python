ca3b199218bbd5fea2390b9c1bd409e8fd938003
Shade <79071277+harkerbyte@users.noreply.github.com>
2023-01-14 02:23:02 +0100

    Update __init__.py

--- bitly_api/__init__.py
+++ bitly_api/__init__.py
@@ -1,6 +1,6 @@
 from bitly_api import Connection, BitlyError, Error
 __version__ = '0.3'
-__author__ = "Jehiah Czebotar <jehiah@gmail.com>"
+__author__ = "shade <cybersalimn@gmail.com>"
 __all__ = ["Connection", "BitlyError", "Error"]
 __doc__ = """
 This is a python library for the bitly api
